85768f01a1bec137939cd751376c9c3510342dfe
sryagit <158165193+sryagit@users.noreply.github.com>
2024-06-14 08:24:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,15 +15,15 @@ def predict_water_potability(ph, hardness, solids, chloramines, sulfate, conduct
 
 def main():
     st.title("Water Potability Web APP")
-    ph			    = st.text_input("ph", placeholder="Type Here") 
-    hardness		= st.text_input("Hardness", placeholder="Type Here")
-    solids			= st.text_input("Solids", placeholder="Type Here")
-    chloramines		= st.text_input("Chloramines", placeholder="Type Here")
-    sulfate			= st.text_input("Sulfate", placeholder="Type Here")
-    conductivity	= st.text_input("Conductivity", placeholder="Type Here")	
-    organic_carbon	= st.text_input("Organic_carbon", placeholder="Type Here")	
-    trihalomethanes	= st.text_input("Trihalomethanes", placeholder="Type Here")	
-    turbidity		= st.text_input("Turbidity", placeholder="Type Here")
+    ph = st.text_input("ph", placeholder="Type Here") 
+    hardness = st.text_input("Hardness", placeholder="Type Here")
+    solids = st.text_input("Solids", placeholder="Type Here")
+    chloramines = st.text_input("Chloramines", placeholder="Type Here")
+    sulfate = st.text_input("Sulfate", placeholder="Type Here")
+    conductivity = st.text_input("Conductivity", placeholder="Type Here")	
+    organic_carbon = st.text_input("Organic_carbon", placeholder="Type Here")	
+    trihalomethanes = st.text_input("Trihalomethanes", placeholder="Type Here")	
+    turbidity = st.text_input("Turbidity", placeholder="Type Here")
 
     if st.button("Get Prediction"):
         output = predict_water_potability(ph, hardness, solids, chloramines, sulfate, conductivity, organic_carbon, trihalomethanes, turbidity)
