51885cfbc63ee624e493b319d2f7647f812c2a5a
sryagit <158165193+sryagit@users.noreply.github.com>
2024-06-14 07:32:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import streamlit as st 
 from PIL import Image
 
-# model = joblib.load('classifier.joblib')
+model = joblib.load('classifier.joblib')
 
 image = Image.open('water.jpg')
 st.image(image.resize((1000, 300)))
@@ -27,10 +27,17 @@ def main():
 
     if st.button("Get Prediction"):
         output = predict_water_potability(ph, Hardness, Solids, Chloramines, Sulfate, Conductivity, Organic_carbon, Trihalomethanes, Turbidity)
-        st.success(f'Result: {output}.')
-        st.write('0 = Water is not Potable (water is not safe for human consumption)')
-        st.write('1 = Water is Potable (water is safe for human consumption)')
+        st.session_state['prediction'] = output
 
+    if 'prediction' in st.session_state:
+        if st.session_state['prediction'] == 0:
+            st.markdown("<h3>Result :<span style='color:red'> 0 </span></h3>", unsafe_allow_html=True)
+        else:
+            st.markdown("<h3>Result :<span style='color:LawnGreen'> 1 </span></h3>", unsafe_allow_html=True)
+            
+        st.markdown("<h5 style='color:red'> 0<span style='color:white'> =</span> Water is Not Potable (i.e. not safe for human consumption)</h5>", unsafe_allow_html=True)
+        st.markdown("<h5 style='color:Blue'> 1<span  style='color:White'> =</span> Water is Potable (i.e. safe for human consumption)</h5>", unsafe_allow_html=True)
+        
     if st.button("About"):
         st.text("Classifier name : Random Forest")
         st.text("Accuracy Score : 70.00")
