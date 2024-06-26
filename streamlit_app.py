74cdfd9132795cfbb8e95b7b86006729ef4ba9af
sryagit <158165193+sryagit@users.noreply.github.com>
2024-06-26 21:31:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 import streamlit as st 
 
-model = joblib.load('waterabc.joblib')
+model = joblib.load('waterdtc2.joblib')
 
 def predict_water_potability(ph, hardness, solids, chloramines, sulfate, conductivity, organic_carbon, trihalomethanes, turbidity):
     prediction = model.predict([[ph, hardness, solids, chloramines, sulfate, conductivity, organic_carbon, trihalomethanes, turbidity]])
