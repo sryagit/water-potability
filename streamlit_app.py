61b0465348fb6509badd02f228d9bc5574aa6ee1
sryagit <158165193+sryagit@users.noreply.github.com>
2024-08-29 16:34:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ def main():
     if st.button("About"):
         st.text("Classifier name : Random Forest")
         st.text("Accuracy Score : 68.00")
-        st.text("Built by : Shamela K")
+        st.text("Built by : Suraj Yadav")
 
 
 if __name__ == '__main__':
